1875c455c5fdb97e545522eeb945f4c90fb2eeac
Sylvain Decout <sylvain.decout+github@gmail.com>
2023-03-05 17:54:05 +0100

    :recycle: Step 5 - drawing on established formalisms

--- src/test/kotlin/fr/sdecout/handson/ordering/domain/invoice/InvoiceLineTest.kt
+++ src/test/kotlin/fr/sdecout/handson/ordering/domain/invoice/InvoiceLineTest.kt
@@ -1,10 +1,11 @@
 package fr.sdecout.handson.ordering.domain.invoice
 
 import fr.sdecout.handson.ordering.domain.DrinkName
-import fr.sdecout.handson.ordering.domain.Money
 import fr.sdecout.handson.ordering.domain.recipe.Quantity.Companion.pieces
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
+import org.joda.money.CurrencyUnit.EUR
+import org.joda.money.Money
 
 class InvoiceLineTest : ShouldSpec({
 
@@ -12,20 +13,20 @@ class InvoiceLineTest : ShouldSpec({
         val invoiceLine = InvoiceLine(
             drink = DrinkName("ESPRESSO"),
             quantity = 3.pieces(),
-            unitPrice = Money(7.00),
+            unitPrice = Money.of(EUR, 7.00),
         )
 
-        invoiceLine.totalPrice shouldBe Money(21.00)
+        invoiceLine.totalPrice shouldBe Money.of(EUR, 21.00)
     }
 
-     should("compute total price with 0 quantity") {
+    should("compute total price with 0 quantity") {
         val invoiceLine = InvoiceLine(
             drink = DrinkName("ESPRESSO"),
             quantity = 0.pieces(),
-            unitPrice = Money(7.00),
+            unitPrice = Money.of(EUR, 7.00),
         )
 
-        invoiceLine.totalPrice shouldBe Money(0.0)
-     }
+        invoiceLine.totalPrice shouldBe Money.zero(EUR)
+    }
 
 })
